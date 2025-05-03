a35f32a4ba7525699fc873b6b804cc462d9ae6d9
kami6618 <kyuce790@gmail.com>
2025-05-03 19:08:02 +0200

    Update main.cpp

--- ADC_register/ADC_exercise/src/main.cpp
+++ ADC_register/ADC_exercise/src/main.cpp
@@ -1,26 +1,45 @@
 #include <Arduino.h>
 
-unsigned long lastmillis = 0;
+#define ADC_CHANNEL 0
+
+volatile int poti_value = 0;
+volatile bool int_flag = false;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  ADMUX = (0000);
+  // ADC aktivieren und Prescaler auf 128 setzen (125 kHz @ 16 MHz)
+  ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+  // Interne 5V Referenzspannung aktivieren und Kanal setzen
+  ADMUX = (ADMUX & 0b11110000) | (ADC_CHANNEL & 0x0F);
   
-  ADCSRA = (1 << ADEN ) | (1<< ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADMUX |= (1 << REFS0);
+
+  sei(); // globale Interrupts aktivieren
+
+  // Erste Wandlung starten
+  ADCSRA |= (1 << ADSC);
 }
 
 void loop() {
+  if (int_flag) {
+    int_flag = false;
 
-  if(millis() - lastmillis >= 500) { // Benutzung von millis für die Aktualisierung alle 0.5 Sekunden
-    
-    ADCSRA |= (1 << ADSC);
-    int analogwert = ADC;
-    float spg = (analogwert * 5.0) /1023.0; // Umrechnung in Spannung
+    float voltage = (poti_value * 5.0) / 1023.0;
+
     Serial.print("Potentiometer analog value = ");
-    Serial.print(spg, 2);
+    Serial.print(voltage, 2);
     Serial.println(" V");
-    
-    lastmillis = millis();
- }
+
+    delay(500);
+
+    // Nächste Messung starten
+    ADCSRA |= (1 << ADSC);
+  }
+}
+
+ISR(ADC_vect) {
+  poti_value = ADC;
+  int_flag = true;
 }
